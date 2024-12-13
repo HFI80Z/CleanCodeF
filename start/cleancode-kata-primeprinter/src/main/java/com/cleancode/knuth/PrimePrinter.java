2a6fe179f62e0cc333aaa93170bc382d27355b68
Esteban <loryesteban@icloud.com>
2024-12-13 09:30:48 +0100

    Second commit

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -1,8 +1,16 @@
 package com.cleancode.knuth;
 
 public class PrimePrinter {
-    public static void main(String[] args) {
-        new PrimePrinterHelper().print();
-    }
 
+    private static final int numberOfNumbers = 1000;
+    private static final int linesPerPage = 50;
+    private static final int columns = 4;
+
+    public static void main(String[] args) {
+        PrimePrinterHelper helper = new PrimePrinterHelper(numberOfNumbers);
+        int[] primes = helper.getPrimes();
+
+        NumberPrinter printer = new NumberPrinter(linesPerPage, columns);
+        printer.printNumbers(primes, numberOfNumbers);
+    }
 }
--- start/cleancode-kata-primeprinter/src/test/java/com/cleancode/knuth/PrimePrinterTest.java
+++ start/cleancode-kata-primeprinter/src/test/java/com/cleancode/knuth/PrimePrinterTest.java
@@ -1,44 +1,37 @@
 package com.cleancode.knuth;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+public class NumberPrinter {
+    private final int linesPerPage;
+    private final int columns;
 
-import java.io.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class PrimePrinterTest {
-    private PrintStream out;
-
-    @BeforeEach
-    void setup() throws Exception {
-        out = System.out;
-        System.setOut(new PrintStream(new FileOutputStream("lead.txt")));
+    public NumberPrinter(int linesPerPage, int columns) {
+        this.linesPerPage = linesPerPage;
+        this.columns = columns;
     }
 
-    @AfterEach
-    void teardown() {
-    	System.out.close();
-        System.setOut(out);
-        new File("lead.txt").delete();
-    }
+    public void printNumbers(int[] numbers, int numberOfNumbers) {
+        int pagenumber = 1;
+        int pageoffset = 1;
 
-    @Test
-    void makeSureOutputMatchesGold() throws Exception {
-        PrimePrinter.main(new String[0]);
-        BufferedReader lead = null;
-        BufferedReader gold = null;
-		try {
-			lead = new BufferedReader(new FileReader("lead.txt"));
-			gold = new BufferedReader(new FileReader("gold.txt"));
-			String line;
-			while ((line = gold.readLine()) != null)
-				assertEquals(line, lead.readLine());
-			assertEquals(null, lead.readLine());
-		} finally {
-			lead.close();
-			gold.close();
-		}
+        while (pageoffset <= numberOfNumbers) {
+            System.out.print("The First ");
+            System.out.print(numberOfNumbers);
+            System.out.print(" Numbers === Page ");
+            System.out.print(pagenumber);
+            System.out.println("\n");
+
+            for (int rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
+                for (int column = 0; column <= columns - 1; column++) {
+                    if (rowoffset + column * linesPerPage <= numberOfNumbers) {
+                        System.out.printf("%10d", numbers[rowoffset + column * linesPerPage]);
+                    }
+                }
+                System.out.println();
+            }
+
+            System.out.println("\f");
+            pagenumber++;
+            pageoffset += linesPerPage * columns;
+        }
     }
 }
