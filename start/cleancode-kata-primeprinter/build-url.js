d2b2f641147bd6d743cf71be58c5abd7e615229f
Esteban <loryesteban@icloud.com>
2024-12-13 09:54:07 +0100

    Ajout du fichier JS

---
+++ start/cleancode-kata-primeprinter/build-url.js
@@ -0,0 +1,31 @@
+function buildUrl(url, options = {}) {
+    let builtUrl = '';
+  
+    if (url === null) {
+      builtUrl = '';
+    } else if (typeof url === 'object') {
+      options = url;
+    } else {
+      builtUrl = url;
+    }
+  
+    if (options.path) {
+      builtUrl += `/${options.path}`;
+    }
+  
+    if (options.queryParams) {
+      const queryString = Object.entries(options.queryParams)
+        .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+        .join('&');
+      if (queryString) {
+        builtUrl += `?${queryString}`;
+      }
+    }
+  
+    if (options.hash) {
+      builtUrl += `#${encodeURIComponent(options.hash)}`;
+    }
+  
+    return builtUrl;
+  }
+  
